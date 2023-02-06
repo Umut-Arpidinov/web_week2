a54b47acd72ee32a2fca45daf020d4c5b6a1d702
Умут Арпидинов <ymytbek0314@gmail.com>
2023-02-06 14:04:22 +0600

    initial commit

---
+++ src/main/java/com/example/springbootapplication/controller/GreetingController.java
@@ -0,0 +1,18 @@
+package com.example.springbootapplication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="noname") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
---
+++ src/main/java/com/example/springbootapplication/model/Person.java
@@ -0,0 +1,28 @@
+package com.example.springbootapplication.model;
+
+public class Person {
+    private String name;
+    private String surname;
+    private long id;
+    private String contactNumber;
+
+    public  String getName() {
+        return name;
+    }
+    public  String getSurname() {
+        return surname;
+    }
+
+    public  long getId() {
+        return id;
+    }
+
+    public  String getName() {
+        return name;
+    }
+
+
+
+}
+
+
